d51181e4c4c9cee5d2faeaf7b3fb14e854a97d5c
Ziyad <zsyz8335@gmai.com>
2025-12-02 10:51:40 +0300

    fronend

--- SaudiEstate/main/views.py
+++ SaudiEstate/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+
+def all_properties(request):
+    return render(request, 'main/all_properties.html')
